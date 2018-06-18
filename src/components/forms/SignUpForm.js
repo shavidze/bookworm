3aec381fbdc9229735888f300902ac13a87fd94d
Your Name <you@example.com>
2018-06-18 19:35:51 +0400

    Implemented Sign Up

---
+++ src/components/forms/SignUpForm.js
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Validator from "validator";
+import { Form, Button } from "semantic-ui-react";
+import InlineError from "../messages/InlineError";
+
+class SignUpForm extends Component {
+  state = {
+    data: {
+      password: "",
+      emai: ""
+    },
+    loading: false,
+    errors: {}
+  };
+  onSubmit = e => {
+    e.preventDefault();
+    const errors = this.validate(this.state.data);
+    this.setState({ errors });
+    if (Object.keys(errors).length === 0) {
+      this.setState({ loading: true });
+      this.props.submit(this.state.data).catch(err => {
+        this.setState({ errors: err.response.data.errors, loading: false });
+      });
+    }
+  };
+
+  onChange = e => {
+    this.setState({
+      data: { ...this.state.data, [e.target.name]: e.target.value }
+    });
+  };
+
+  validate = data => {
+    const errors = {};
+    if (!Validator.isEmail(data.email)) errors.email = "Invalid email";
+    if (!data.password) errors.password = "Can't be blank";
+    return errors;
+  };
+
+  render() {
+    const { data, errors, loading } = this.state;
+    return (
+      <Form onSubmit={this.onSubmit} loading={loading}>
+        <Form.Field error={!!errors.email}>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="email@email.com"
+            value={data.email}
+            onChange={this.onChange}
+          />
+          {errors.email && <InlineError text={errors.emai} />}
+        </Form.Field>
+        <Form.Field error={!!errors.password}>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Make it secure"
+            value={data.password}
+            onChange={this.onChange}
+          />
+          {errors.password && <InlineError text={errors.password} />}
+        </Form.Field>
+        <Button primary>Sign Up</Button>
+      </Form>
+    );
+  }
+}
+
+SignUpForm.propTypes = {
+  submit: PropTypes.func.isRequired
+};
+
+export default SignUpForm;
---
+++ src/components/pages/SignUpPage.js
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { signup } from "../../actions/users";
+import SignUpForm from "../forms/SignUpForm";
+
+class SignUpPage extends React.Component {
+  submit = data =>
+    this.props.signup(data).then(() => {
+      console.log(this.props.history);
+      this.props.history.push("/dashboard");
+      console.log(this.props.history);
+    });
+
+  render() {
+    return (
+      <div>
+        <SignUpForm submit={this.submit} />
+      </div>
+    );
+  }
+}
+
+SignUpPage.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired
+  }).isRequired,
+  signup: PropTypes.func.isRequired
+};
+export default connect(
+  null,
+  { signup }
+)(SignUpPage);
